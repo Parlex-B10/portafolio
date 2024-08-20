0d0be583faa86f69680dd2aac225c3698aab91fb
Parlex-B10 <83713872+Parlex-B10@users.noreply.github.com>
2024-08-20 10:02:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ if sm=='Español':
         
         def show_pdf(file_path):
             with open(file_path, "rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
             pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
     
